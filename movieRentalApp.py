c5c7756c0610542dda3f0017577552419e535fb0
Maryam Elhamidi <unicolmaryame@gmail.com>
2023-10-18 11:21:13 -0400

    project setup complete

---
+++ movieRentalApp.py
@@ -0,0 +1,10 @@
+import movieStore
+
+class MovieRentalApp:
+    def run(self):
+        return print("Movie Rental Store - Maryam")
+    
+    def setup(self):
+        return print("Project Setup Complete.")
+    
+
