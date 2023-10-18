2e728a6f473904657c1ee2b7a753db820e5b3db8
Maryam Elhamidi <unicolmaryame@gmail.com>
2023-10-18 11:25:30 -0400

    Moviestore constructor created

--- movieStore.py
+++ movieStore.py
@@ -1,1 +1,8 @@
 class MovieStore:
+    def __init__(self, name, num_movie, avail_movies, rent_fee, late_fee):
+        self.self = self
+        self.name = name
+        self.num_movie = num_movie
+        self.avail_movies = avail_movies
+        self.rent_fee = rent_fee
+        self.late_fee = late_fee
