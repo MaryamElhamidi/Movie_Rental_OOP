29d510bb399b743c8c32876d142f9c2b989f4021
Maryam Elhamidi <unicolmaryame@gmail.com>
2023-10-18 13:05:22 -0400

    Completed/done

--- movieStore.py
+++ movieStore.py
@@ -9,15 +9,15 @@ class MovieStore:
         self.late_fee = late_fee
 #Part 2.2 Define accessors and mutators
     def getName(self):
-        return self.__name
+        return self.name
     def getMovNum(self):
-        return self.__num_movie
+        return self.num_movie
     def getAvailMov(self):
-        return self.__avail_movies
+        return self.avail_movies
     def getRentFee(self):
-        return self.__rent_fee
+        return self.rent_fee
     def getLateFee(self):
-        return self.__late_fee    
+        return self.late_fee    
     
 
     def setRentFee(self):
@@ -26,24 +26,24 @@ class MovieStore:
         
     def calculateLateFee(self):
         days_overdue = float(input("Enter the number of days the movie is overdue: "))
-        lateFee_Overdue = self.__late_fee * days_overdue
+        lateFee_Overdue = self.late_fee * days_overdue
         return print(f"The calculated late fee is {lateFee_Overdue()}")
    
     def rentMovie(self):
         selection = int(input("Request a number of movies: "))
-        if selection > self.__avail_movies:
+        if selection > self.avail_movies:
             print("Invalid. The requested number of movies is greater than the available movies.")
             return False
         else:
-            update = self.__avail_movies - selection
+            update = self.avail_movies - selection
             print("The updated number of available movies is: ", update)
             return True
 
     def returnMovie(self):
         movie_returns = input("Enter the amount of movies you are returning: ")
-        new_avail_movies = movie_returns + self.__avail_movies
+        new_avail_movies = movie_returns + self.avail_movies
         days_overdue = float(input("Enter the number of days the movie is overdue: "))
-        lateFee_Overdue = self.__late_fee * days_overdue
+        lateFee_Overdue = self.late_fee * days_overdue
         if days_overdue > 0:
             print("Please pay your late fee. It is: ",lateFee_Overdue)
         else:
