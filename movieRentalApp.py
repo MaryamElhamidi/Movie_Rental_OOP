544991912ed142b9d521b7613d539798ef350568
Maryam Elhamidi <unicolmaryame@gmail.com>
2023-10-18 12:23:54 -0400

    Created methods for renting and returning movies

--- movieRentalApp.py
+++ movieRentalApp.py
@@ -2,9 +2,25 @@ import movieStore
 
 class MovieRentalApp:
     def run(self):
-        return print("Movie Rental Store - Maryam")
+        return print(" Welcome to Maryam's Movie Rental Store")
     
     def setup(self):
         return print("Project Setup Complete.")
     
+"""1. (8 Points) Create an object of the "MovieStore" class by accepting the required data from the end- user.
+2. (2 Points) Print the details of the movie store, including the store name, total number of movies, rental fee per movie, and late fee per day.
+Commit the changes done so far, with a message “Created instance of MovieStore”
+3. (8 Points) Ask the user if they wish to rent or return movies.
+a. For renting, accept the number of movies to rent and update the available movies. Display the total fee due if renting was successful. If renting is not successful display, show an appropriate error message.
+b. For returning, accept the number of movies returned, and if any movies returns are overdue, calculate and display the late fee.
+4. (2 Points) Update the movie rental fee to 75% of the original price.
+Commit the changes done so far, with a message “User interaction implemented.”"""
 
+user1 = ("maryam", 40, 15, 2.99, 0.99)
+#Bonus
+class Bonus:
+    def userList(self):
+        return
+"""present a menu to the user with three options of 
+renting movies, return movies, calculating late fees, 
+and exiting"""
--- movieStore.py
+++ movieStore.py
@@ -1,8 +1,50 @@
 class MovieStore:
+#Part 2.1 Creating initializer
     def __init__(self, name, num_movie, avail_movies, rent_fee, late_fee):
-        self.self = self
-        self.name = name
-        self.num_movie = num_movie
-        self.avail_movies = avail_movies
-        self.rent_fee = rent_fee
-        self.late_fee = late_fee
+        self.__self = self
+        self.__name = str(name)
+        self.__num_movie = int(num_movie)
+        self.__avail_movies = int(avail_movies)
+        self.__rent_fee = float(rent_fee)
+        self.__late_fee = float(late_fee)
+#Part 2.2 Define accessors and mutators
+    def getName(self):
+        return self.__name
+    def getMovNum(self):
+        return self.__num_movie
+    def getAvailMov(self):
+        return self.__avail_movies
+    def getRentFee(self):
+        return self.__rent_fee
+    def getLateFee(self):
+        return self.__late_fee    
+    
+
+    def setRentFee(self):
+        if self.rent_fee < 0:
+            return print("Rejected: Invalid Fee")
+        
+    def calculateLateFee(self):
+        days_overdue = float(input("Enter the number of days the movie is overdue: "))
+        lateFee_Overdue = self.__late_fee * days_overdue
+        return print(f"The calculated late fee is {lateFee_Overdue()}")
+   
+    def rentMovie(self):
+        selection = int(input("Request a number of movies: "))
+        if selection > self.__avail_movies:
+            print("Invalid. The requested number of movies is greater than the available movies.")
+            return False
+        else:
+            update = self.__avail_movies - selection
+            print("The updated number of available movies is: ", update)
+            return True
+
+    def returnMovie(self):
+        movie_returns = input("Enter the amount of movies you are returning: ")
+        new_avail_movies = movie_returns + self.__avail_movies
+        days_overdue = float(input("Enter the number of days the movie is overdue: "))
+        lateFee_Overdue = self.__late_fee * days_overdue
+        if days_overdue > 0:
+            print("Please pay your late fee. It is: ",lateFee_Overdue)
+        else:
+            print("Thank you. See you again.")
